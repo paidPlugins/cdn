a5e1171ab638efb96347003777300749ab4c2a16
legendSabbir <mmax206602@gmail.com>
2023-09-29 16:26:53 +0000

    terminal-pro-live-server

--- installer.sh
+++ installer.sh
@@ -1,18 +1,3 @@
-pkg install -y make python3 build-essential nodejs
+apt install -y make python3 build-essential nodejs
 
-rm -rf ~/.terminal-pro
-mkdir ~/.terminal-pro
-
-cd ~/.terminal-pro
-
-npm i node-pty ws
-
-curl "https://raw.githubusercontent.com/paidPlugins/cdn/main/server.js" -o server.js 
-
-if [ ! -f ~/.bashrc ]; then
-  touch ~/.bashrc
-fi
-
-if ! grep -q 'alias start-terminal="node ~/.terminal-pro/server"' ~/.bashrc; then
-  echo 'alias start-terminal="node ~/.terminal-pro/server"' >> ~/.bashrc
-fi
+npm i -g terminal-pro

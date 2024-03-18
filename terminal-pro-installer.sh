47f55a7894b0aae7bd5b121923fac7ce04f3a78d
legendSabbir <mmax206602@gmail.com>
2024-03-19 05:20:05 +0600

    installer

--- terminal-pro-installer.sh
+++ terminal-pro-installer.sh
@@ -1,19 +1,14 @@
 apt install -y make python3 build-essential nodejs
+mkdir -p ~/.gyp && echo "{'variables': {'android_ndk_path': ''}}" > ~/.gyp/include.gy
 
 npm uninstall -g terminal-pro
 rm -rf ~/.terminal-pro
 mkdir ~/.terminal-pro
 cd ~/.terminal-pro
 
-curl https://raw.githubusercontent.com/paidPlugins/cdn/main/configure.js -o configure.js
-npm i node-gyp
-mv configure.js node_modules/node-gyp/lib/configure.js
-
-npm i node-pty
-npm i ws
-npm i commander
-
 curl https://raw.githubusercontent.com/paidPlugins/cdn/main/terminal-pro-package.json -o package.json
+npm install
+
 curl https://raw.githubusercontent.com/paidPlugins/cdn/main/server.js -o index.js
 
 chmod +x index.js
